277d90f531b63b6afa142c3531b34015b2026f51
Akbarkhon <akbarkhonavazkhonov@gmail.com>
2024-08-23 18:00:40 +0500

    beautify

--- src/components/Login/FormLogin.jsx
+++ src/components/Login/FormLogin.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./Login.scss";
-import { useNavigate } from "react-router-dom";
-import { APP_ROUTES } from "../../router/Route";
+import {useNavigate} from "react-router-dom";
+import {APP_ROUTES} from "../../router/Route";
 import axios from "axios";
 
-import { ToastContainer, toast } from "react-toastify";
+import {ToastContainer, toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import logo from "../../images/logo.svg";
 
 function FormLogin() {
-  const [loginInput, setLoginInput] = useState("");
-  const [passInput, setpassInput] = useState("");
+  const [loginInput,
+    setLoginInput] = useState("");
+  const [passInput,
+    setpassInput] = useState("");
   const url = `${APP_ROUTES.URL}/admin/login`;
   const navigation = useNavigate();
 
-  const handleLogin = async () => {
+  const handleLogin = async() => {
     try {
       const response = await axios.post(url, {
         name: loginInput,
-        password: passInput,
+        password: passInput
       });
 
       const token = response.data.access_token;
-      
+
       if (token) {
         localStorage.setItem("@token", token);
         localStorage.setItem("@role", response.data.role);
         if (response.data.role === "admin") {
-          navigation(APP_ROUTES.COURSE, { replace: true });
+          navigation(APP_ROUTES.COURSE, {replace: true});
         } else if (response.data.role === "mentor") {
-          navigation(APP_ROUTES.CHAT, { replace: true });
+          navigation(APP_ROUTES.CHAT, {replace: true});
         } else if (response.data.role === "finance") {
-          navigation(APP_ROUTES.FINANCE, { replace: true });
+          navigation(APP_ROUTES.FINANCE, {replace: true});
         } else {
           toast.error("Не верный логин или пароль");
         }
@@ -41,11 +43,25 @@ function FormLogin() {
     }
   };
 
+  //on enter press login
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleLogin();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keypress", handleKeyPress);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    }
+  });
+  
   return (
     <div className="modalWrapper">
-      <ToastContainer />
+      <ToastContainer/>
       <div className="headerComponents">
-        <img src={logo} alt={logo} />
+        <img src={logo} alt={logo}/>
       </div>
       <div className="formWrapper">
         <div className="inputElement">
@@ -55,8 +71,7 @@ function FormLogin() {
             id="login"
             value={loginInput}
             onChange={(e) => setLoginInput(e.target.value)}
-            name="login"
-          />
+            name="login"/>
         </div>
         <div className="inputElement">
           <input
@@ -65,8 +80,7 @@ function FormLogin() {
             id="pass"
             value={passInput}
             onChange={(e) => setpassInput(e.target.value)}
-            name="pass"
-          />
+            name="pass"/>
         </div>
         <div className="sendDataWrapper">
           <button className="sendData" onClick={handleLogin}>
